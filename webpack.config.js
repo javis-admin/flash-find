59f749cfe16979fe76882fbdb1c12aecd9c4e8d0
prashant3012 <thakur.peeyush@javis.ai>
2024-07-19 13:48:56 +0530

    poc

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", // Adjust this to the correct entry point of your application
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.worker\.js$/,
+        use: {
+          loader: "worker-loader",
+          options: { inline: "no-fallback" },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+};
